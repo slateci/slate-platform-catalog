414b03c624bdcbf0f8076d2db9a2759846312d60
Gabriele Carcassi <carcassi@umich.edu>
2019-05-17 08:59:55 -0400

    monitoring: renaming site to cluster and moving over cluster configurations

--- incubator/monitoring/bin/list-thanos-ip.sh
+++ incubator/monitoring/bin/list-thanos-ip.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for SITE in `./list-sites.sh` ; do
+for SITE in `./list-clusters.sh` ; do
     KUBECONFIG=~/conf/$SITE.conf
     echo IP for $SITE:
     kubectl get service thanos-store --namespace slate-monitoring --kubeconfig $KUBECONFIG --output=custom-columns=IP:.status.loadBalancer.ingress[*].ip --no-headers=true 
